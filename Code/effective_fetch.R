3aca5dd82abfa94f9b296cd467d7f5da9c5cea2c
cole-fields <cole.fields@dfo-mpo.gc.ca>
2023-11-10 15:03:18 -0800

    cleanup

--- Code/effective_fetch.R
+++ Code/effective_fetch.R
@@ -8,7 +8,7 @@ library(vroom)
 # Controls ----
 
 # relative contribution of fetch vectors surrounding each heading to calculation of effective fetch
-weights <- cos(c(45,33.75,22.5,11.25,0,11.25,22.5,33.75,45) *(pi/180))
+weights <- cos(c(seq(45, 0, by = -5), seq(5, 45, by = 5)) * (pi/180))
 
 # Path to project directory (string)
 data_dir <- file.path('D:/projects/REI-WaveExp/data', 'hg')
@@ -59,87 +59,9 @@ fetch_eff <- fetch_summary %>%
   nest() %>% 
   bind_cols(., site_names = as.character(seq_len(nrow(.)))) %>%   
   # Calculate effective fetch and place in tibble
-  mutate(fetch_eff = map(data, ~roll.recycle(.$fetch_length_m, 9, 8, by = 9)),
+  mutate(fetch_eff = map(data, ~roll.recycle(.$fetch_length_m, 19, 8, by = 9)),
          fetch_eff = map(fetch_eff, ~as.vector((weights %*% .)/sum(weights))),
          fetch_eff = map(fetch_eff, ~tibble(direction = c(360, seq(45, 315, 45)), fetch = .))) %>% 
   st_as_sf(coords = c("X","Y"), crs = crs_fetch)
 
 saveRDS(fetch_eff, file.path(output_dir, "fetch_effective.rds"))
-
-#######################################################################
-##########Environmental Data Layers for SG sdm#########################
-#######################################################################
-
-#### Load Packages ####
-
-library(dplyr)
-library(purrr)
-library(tidyr)
-library(sf)
-library(terra)
-
-# Housekeeping ----
-
-# Read in interpolated wind data ----
-# pwd = getwd()
-data_dir <- file.path('D:/projects/REI-WaveExp/data/hg/v1.5')
-tif_dir <- file.path('E:/HRDPS2.5/results/hg/v1.1', 'spline_hrdps')
-
-# Raster stack of wind frequency by direction
-wind_freq <- list.files(path = tif_dir,
-                        pattern = "^freq.+tif$",
-                        full.names = T) %>% 
-  rast(.) 
-
-# Raster stack of average daily maximum wind speed by direction
-
-wind_spd <- list.files(path=tif_dir,
-                       pattern="^mx.+tif$",
-                       full.names=T) %>% 
-  rast(.)
-
-# Combine fetch data and wind data ----
-
-# Read in effective fetch data
-# fetch <- terra::vect(file.path(data_dir, 'fetch_barkeley.shp'))
-fetch_eff <- readRDS(file.path(data_dir, "fetch_effective.rds"))
-
-# Extract frequency and speed values for fetch locations
-
-freq_values <- terra::extract(x=wind_freq, 
-                              y=fetch_eff)
-spd_values <- terra::extract(x=wind_spd, 
-                             y=fetch_eff)
-wind_combine <- merge(freq_values, spd_values, by="ID")
-
-# Calculate Relative Exposure Index ----
-expo_by_site <- wind_combine %>%
-  rowwise() %>% 
-  mutate(freq_total = list(cbind(c_across(contains("freq")))),
-         max_wind = list(cbind(c_across(contains("mx")))),
-         wind_dir_freq = list(tibble(max_wind, 
-                                     freq_total, 
-                                     direction = c(135,180,225,270,315,360,45,90)))) %>%
-  ungroup() %>% 
-  mutate(., wind_dir_freq = map(wind_dir_freq, function(.data) {
-    .data %>% 
-      dplyr::arrange(., direction)
-  }),
-  fetch_eff = map(fetch_eff$fetch_eff, function(.data){
-    .data %>% 
-      dplyr::arrange(., direction)
-  })) %>% 
-  mutate(REI = map2_dbl(wind_dir_freq, fetch_eff, # calculate relative exposure
-                        ~sum((.x$max_wind*.x$freq_total*.y$fetch), na.rm = T))) %>%  
-  dplyr::select(ID, REI) %>% # keep site, REI, and geometry
-  rename(., site = ID) %>%
-  mutate(geometry = fetch_eff$geometry)
-
-# Save REI object
-saveRDS(expo_by_site, file.path(data_dir, 'REI.rds'))
-
-# Write to csv
-st_write(obj=expo_by_site,
-         dsn=file.path(data_dir, 'REI.csv'),
-         layer_options='GEOMETRY=AS_XY',
-         append=FALSE)
--- Code/roll_recycle_fun.R
+++ Code/roll_recycle_fun.R
@@ -4,7 +4,7 @@
 
 roll.recycle <- function(vec, size, n, by=1, index=FALSE){
   vec.length <- length(vec)
-  vals <- (seq(from = 1-floor(size/2), by = 1, length.out = 9) + rep(seq(from=0, by=by, length.out=n), each=size))%%vec.length
+  vals <- (seq(from = 1-floor(size/2), by = 1, length.out = size) + rep(seq(from=0, by=by, length.out=n), each=size))%%vec.length
   use.inds <- matrix(vals, nrow=size)
   use.inds[use.inds==0] <- vec.length
   if(index){
